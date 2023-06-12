ac63239278b52902ac8543b8fbc96c29f718e782
masaya80 <shimoda.masaya@cama.jp>
2023-06-12 16:19:59 +0900

    test: husky-prestaged

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,11 @@ import * as React from "react";
 
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 
 import { Header } from "@/shared/components/navigation/Header/Header";
+
+import Typography from "@mui/material/Typography";
+
 
 export default function Home() {
   return (
